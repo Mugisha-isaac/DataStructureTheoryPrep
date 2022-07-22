550fcec053c4f44c9c4e5319f0fec3ab52bd4859
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-07-23 01:16:15 +0200

    [feat] sorting linked lisgg

--- LinkedList.cc
+++ LinkedList.cc
@@ -1,102 +1,141 @@
-#include<iostream>
-#include<stdlib.h>
-
+#include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
-struct Node{
+struct Node
+{
     int data;
-    struct Node * next;
+    struct Node *next;
 };
 
-
-void insertAtBeginning(struct Node** head, int  data){
-     struct Node* newNode = new Node;
-     newNode->data =data;
-     newNode->next = (*head);
-     *head = newNode;
+void insertAtBeginning(struct Node **head, int data)
+{
+    struct Node *newNode = new Node;
+    newNode->data = data;
+    newNode->next = (*head);
+    *head = newNode;
 }
 
-void print(struct Node* node){
-    while (node !=NULL)
+void print(struct Node *node)
+{
+    while (node != NULL)
     {
-        cout<<node->data<<" ";
+        cout << node->data << " ";
         node = node->next;
     }
-    cout<<endl;
+    cout << endl;
 }
 
-void insertAfterNode(struct Node * prev, int data){
-    if(prev == NULL){
-        cout<<"Prev node should not be null"<<endl;
+void insertAfterNode(struct Node *prev, int data)
+{
+    if (prev == NULL)
+    {
+        cout << "Prev node should not be null" << endl;
         return;
     }
 
-    struct  Node* newNode = new Node;
+    struct Node *newNode = new Node;
     newNode->data = data;
     newNode->next = prev->next;
     prev->next = newNode;
 }
 
-void searchNode(struct Node** head, int data){
+void searchNode(struct Node **head, int data)
+{
     bool flag = false;
-    struct Node* curr = *head;
+    struct Node *curr = *head;
     while (curr != NULL)
     {
-        if(curr->data == data){
-           flag = true;
-           break;
+        if (curr->data == data)
+        {
+            flag = true;
+            break;
         }
         curr = curr->next;
-        flag=false;
+        flag = false;
     }
 
-    if(flag == true){
-        cout<<data<<" Data is Found!"<<endl;
+    if (flag == true)
+    {
+        cout << data << " Data is Found!" << endl;
     }
-    else{
-       cout<<data<<" Data is Not Found!"<<endl;
+    else
+    {
+        cout << data << " Data is Not Found!" << endl;
     }
-    
 }
 
-
-void deleteNode(struct Node**head, int key){
-      struct Node* curr = *head,*prev;
-      if(curr!=NULL && curr->data == key){
-          *head = curr->next;
-          free(curr);
-          cout<<" Head is deleted"<<endl;
-      }
-      else{
-          while (curr!=NULL && curr->data != key)
-          {
+void deleteNode(struct Node **head, int key)
+{
+    struct Node *curr = *head, *prev;
+    if (curr != NULL && curr->data == key)
+    {
+        *head = curr->next;
+        free(curr);
+        cout << " Head is deleted" << endl;
+    }
+    else
+    {
+        while (curr != NULL && curr->data != key)
+        {
             prev = curr;
             curr = curr->next;
-          }
+        }
 
-          if(curr==NULL){
+        if (curr == NULL)
+        {
             return;
-          }
-          else{
+        }
+        else
+        {
             prev->next = curr->next;
             free(curr);
-            cout<<"Node Deleted Successfully"<<endl;
-          }
-          
-      }
-      
-      
+            cout << "Node Deleted Successfully" << endl;
+        }
+    }
 }
 
-int main(){
-    struct Node* head = NULL;
-    insertAtBeginning(&head,2);
-    insertAtBeginning(&head,1);
-    insertAtBeginning(&head,0);
-    insertAfterNode(head->next->next,10);
+void sortLinkedList(struct Node**head){
+    struct Node* curr = *head,*index=NULL;
+    int temp;
+
+    if(head==NULL){
+        return;
+    }
+    else{
+        while (curr !=NULL)
+        {
+            index = curr->next;
+
+            while (index !=NULL)
+            {
+               if(curr->data > index->data){
+                temp = curr->data;
+                curr->data = index->data;
+                index->data = temp;
+               }
+               else{
+                 index = index->next;
+               }
+            }
+            curr = curr->next;
+        }
+        
+    }
+}
+
+int main()
+{
+    struct Node *head = NULL;
+    insertAtBeginning(&head, 2);
+    insertAtBeginning(&head, 1);
+    insertAtBeginning(&head, 0);
+    insertAfterNode(head->next, 10);
     // searchNode(&head,10);
     print(head);
-    deleteNode(&head,2);
-     print(head);
+    // deleteNode(&head, 2);
+    sortLinkedList(&head);
+    cout<<" Sorted Linked List is: "<<endl;
+    print(head);
 }
