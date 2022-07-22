99c8d0a5244b3b60735c006b29dc0a05afe81595
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-07-22 11:48:01 +0200

    [feat] calculating average of array elements

--- insertAndPrint.cc
+++ insertAndPrint.cc
@@ -10,19 +10,26 @@ void insertAndPrintArray(){
     int arr [20] = {};
     int marks;
     int sum =0;
+    int count = 0;
+    int average =0;
 
     for(int i=0; i<5; i++){
         cout<<"Enter "<<i<<" marks "<<endl;
         cin>>marks;
         arr[i] = marks;
         sum = sum + arr[i];
+        count ++;
     }
 
     for(int m=0;m<5;m++){
         cout<<arr[m]<<endl;
     }
 
+    average = sum/count;
+
     cout<< "Sum of elements in array is: "<<sum<<endl;
+    cout<<"Counter is: "<<count<<endl;
+    cout<<"Average is: "<< average<<endl;
 
 }
 
