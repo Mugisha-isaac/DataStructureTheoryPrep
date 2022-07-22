d83998c0590629f2de332414900a508bbe0da710
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-07-22 11:43:01 +0200

    [feat] adding elements from the array

--- insertAndPrint.cc
+++ insertAndPrint.cc
@@ -9,16 +9,20 @@ void insertAndPrintArray(){
 
     int arr [20] = {};
     int marks;
+    int sum =0;
 
     for(int i=0; i<5; i++){
         cout<<"Enter "<<i<<" marks "<<endl;
         cin>>marks;
         arr[i] = marks;
+        sum = sum + arr[i];
     }
 
     for(int m=0;m<5;m++){
         cout<<arr[m]<<endl;
     }
+
+    cout<< "Sum of elements in array is: "<<sum<<endl;
 
 }
 
